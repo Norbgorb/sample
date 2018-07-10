899d31a8dea62571378851549df1cf5844af8e23
xiaofan0208 <xiaofan0208@126.com>
2018-07-10 22:28:05 +0800

    Finish static pages

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/about', 'StaticPagesController@about');
+Route::get('/','StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+
